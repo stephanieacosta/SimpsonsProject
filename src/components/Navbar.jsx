01b2327ff3a0de69da61230b2abd5ff083aea9bc
stephanieacosta <stephaniemariaacosta@gmail.com>
2024-03-04 20:53:33 -0500

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <header className="headernav">
       <figure className="imgnav">
-        <img src="https://s3-alpha-sig.figma.com/img/0f1a/0e19/4dda52df75c902bf4e9170ec93df0c1c?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=C6RyDHtOuW0E6oj7aq5QEv7yypoqYuHkrIRXe1iUKhdh0SN2z68Gy8qVhUUZXjEe~NhGGBB-RVtg4OadDz~ALl1zIiK0qQaauZlOuK3Jvy-sp-0eJ46FYDG8liFOB2exRhp-nFeLaRn-tqBVfAk~aCqA1OxcjYojDKSA4x0qLb8p7mcNt4ih-L~g1W~V9dNcBlgSdSMYSea3Hs-zoZrf7sUslo0-BZAGBCBcqZhAQQ3cFIevW8ujcJ0m8UkXgJcHOxX10hQyBc3ng5icHY2PRHJWc4gdRqu9TQgFfcWr9BYIIFaq-GqsDkqa7in9aN5yPJq8u~5F9pzwNo0EH7fxiQ__" />
+        <img src="\LogoSimpsons.png" alt="LogoSimpsons" />
       </figure>
       <nav role="navigation">
         <ul className="nav-links">
